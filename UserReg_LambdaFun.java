798d90052df3f39484157f24d26b16a1ec8e4c8a
kanchan-solanke <kanchanssolanke@gmail.com>
2021-08-08 23:37:17 +0530

    Email is Valid or not

--- UserReg_LambdaFun.java
+++ UserReg_LambdaFun.java
@@ -11,6 +11,7 @@ interface UserIsValid {
 public class UserReg_LambdaFun {
     static String firstnamePattern = "^[A-Z]{1}+[a-z]{3,}$";
     static String lastnamePattern = "^[A-Z]{1}+[a-z]{3,}$";
+    static String emailPattern ="^[a-zA-Z0-9.+_-]+[@][a-zA-Z0-9]+[.]co(m|.in)$";
 
     // create an object of interface
     UserIsValid checkValid = (String pattern, String name) -> Pattern.matches(pattern, name);
@@ -33,6 +34,15 @@ public class UserReg_LambdaFun {
         }
     }
 
+    // method to check email is valid or not
+    public void emailIsValid(String email) {
+        if (checkValid.mathPattern(emailPattern, email)) {
+            System.out.println("Email is Valid");
+        } else {
+            System.out.println("Please Enter a Valid Email : ");
+        }
+    }
+
     // main function
     public static void main(String[] args) {
         UserReg_LambdaFun lambda = new UserReg_LambdaFun();  //create an object
@@ -46,5 +56,10 @@ public class UserReg_LambdaFun {
         System.out.println("Enter last name:");
         String lname = sc.nextLine();
         lambda.lastnameIsValid(lname);
+
+        //For Email
+        System.out.println("Enter Email :");
+        String email = sc.nextLine();
+        lambda.emailIsValid(email);
     }
 }
