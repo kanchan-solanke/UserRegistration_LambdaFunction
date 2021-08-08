81974bc246a39c4d0e0da842c86d353f5bbc911d
kanchan-solanke <kanchanssolanke@gmail.com>
2021-08-08 21:30:33 +0530

    Last Name is Valid or not

--- UserReg_LambdaFun.java
+++ UserReg_LambdaFun.java
@@ -8,6 +8,7 @@ interface UserIsValid {
 
 public class UserReg_LambdaFun {
     static String namePattern = "^[A-Z]{1}+[a-z]{3,}$";
+    static String lastPattern = "^[A-Z]{1}+[a-z]{3,}$";
     UserIsValid checkValid = (String pattern, String name) -> Pattern.matches(pattern, name);
 
     public void nameIsValid(String name){
@@ -18,11 +19,25 @@ public class UserReg_LambdaFun {
             }
     }
 
+    public void lastIsValid(String email){
+        if(checkValid.mathPattern(lastPattern, email)) {
+            System.out.println("Last name is Valid");
+        }
+        else {
+            System.out.println("Please Enter a Valid last name:");
+        }
+    }
     public static void main(String[] args) {
         UserReg_LambdaFun lambda = new UserReg_LambdaFun();
         Scanner sc = new Scanner(System.in);
+        // For First Name
         System.out.println("Enter first name:");
         String fname = sc.nextLine();
         lambda.nameIsValid(fname);
+
+        // for Last Name
+        System.out.println("Enter Last name");
+        String lname = sc.nextLine();
+        lambda.lastIsValid(lname);
     }
         }
