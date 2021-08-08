5f4043589a9f130e0c425f8a399dd302594ec2dd
kanchan-solanke <kanchanssolanke@gmail.com>
2021-08-08 23:25:57 +0530

    Last Name is Valid or not

--- UserReg_LambdaFun.java
+++ UserReg_LambdaFun.java
@@ -1,16 +1,22 @@
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
+// Create Interface
 @FunctionalInterface
 interface UserIsValid {
     boolean mathPattern(String pattern, String name);
 }
 
+// class
 public class UserReg_LambdaFun {
     static String firstnamePattern = "^[A-Z]{1}+[a-z]{3,}$";
+    static String lastnamePattern = "^[A-Z]{1}+[a-z]{3,}$";
+
+    // create an object of interface
     UserIsValid checkValid = (String pattern, String name) -> Pattern.matches(pattern, name);
 
-    public void nameIsValid(String name) {
+    //method to check firstname is valid or not
+    public void firstnameIsValid(String name) {
         if (checkValid.mathPattern(firstnamePattern, name)) {
             System.out.println("Name is Valid");
         } else {
@@ -18,12 +24,27 @@ public class UserReg_LambdaFun {
         }
     }
 
+    // method to check last name is valid or not
+    public void lastnameIsValid(String name) {
+        if (checkValid.mathPattern(lastnamePattern, name)) {
+            System.out.println("Name is Valid");
+        } else {
+            System.out.println("Please Enter a Valid Last name: ");
+        }
+    }
+
+    // main function
     public static void main(String[] args) {
-        UserReg_LambdaFun lambda = new UserReg_LambdaFun();
+        UserReg_LambdaFun lambda = new UserReg_LambdaFun();  //create an object
         Scanner sc = new Scanner(System.in);
         // For First Name
         System.out.println("Enter first name:");
         String fname = sc.nextLine();
-        lambda.nameIsValid(fname);
+        lambda.firstnameIsValid(fname);
+
+        //For Last Name
+        System.out.println("Enter last name:");
+        String lname = sc.nextLine();
+        lambda.lastnameIsValid(lname);
     }
 }
